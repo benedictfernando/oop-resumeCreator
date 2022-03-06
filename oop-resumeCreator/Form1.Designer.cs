1514e162b48f470a93e9a87610596b24071ce112
benedictfernando <fernandobenedict619@gmail.com>
2022-03-06 15:11:42 +0800

    generate user interface

--- oop-resumeCreator/Form1.Designer.cs
+++ oop-resumeCreator/Form1.Designer.cs
@@ -1,6 +1,6 @@
 ﻿namespace oop_resumeCreator
 {
-    partial class Form1
+    partial class resume
     {
         /// <summary>
         ///  Required designer variable.
@@ -28,12 +28,67 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.components = new System.ComponentModel.Container();
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(resume));
+            this.tableLayoutPanel1 = new System.Windows.Forms.TableLayoutPanel();
+            this.load = new System.Windows.Forms.Button();
+            this.generate = new System.Windows.Forms.Button();
+            this.tableLayoutPanel1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // tableLayoutPanel1
+            // 
+            this.tableLayoutPanel1.ColumnCount = 2;
+            this.tableLayoutPanel1.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Percent, 50F));
+            this.tableLayoutPanel1.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Percent, 50F));
+            this.tableLayoutPanel1.Controls.Add(this.load, 0, 0);
+            this.tableLayoutPanel1.Controls.Add(this.generate, 1, 0);
+            this.tableLayoutPanel1.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.tableLayoutPanel1.Location = new System.Drawing.Point(0, 0);
+            this.tableLayoutPanel1.Name = "tableLayoutPanel1";
+            this.tableLayoutPanel1.RowCount = 1;
+            this.tableLayoutPanel1.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 50F));
+            this.tableLayoutPanel1.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 50F));
+            this.tableLayoutPanel1.Size = new System.Drawing.Size(284, 161);
+            this.tableLayoutPanel1.TabIndex = 0;
+            // 
+            // load
+            // 
+            this.load.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.load.Location = new System.Drawing.Point(3, 3);
+            this.load.Name = "load";
+            this.load.Size = new System.Drawing.Size(136, 155);
+            this.load.TabIndex = 0;
+            this.load.Text = "Load JSON";
+            this.load.UseVisualStyleBackColor = true;
+            // 
+            // generate
+            // 
+            this.generate.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.generate.Location = new System.Drawing.Point(145, 3);
+            this.generate.Name = "generate";
+            this.generate.Size = new System.Drawing.Size(136, 155);
+            this.generate.TabIndex = 1;
+            this.generate.Text = "Generate Resume";
+            this.generate.UseVisualStyleBackColor = true;
+            // 
+            // resume
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 15F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(800, 450);
-            this.Text = "Form1";
+            this.ClientSize = new System.Drawing.Size(284, 161);
+            this.Controls.Add(this.tableLayoutPanel1);
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.Name = "resume";
+            this.Text = "C# Resume Creator";
+            this.tableLayoutPanel1.ResumeLayout(false);
+            this.ResumeLayout(false);
+
         }
 
         #endregion
+
+        private System.Windows.Forms.TableLayoutPanel tableLayoutPanel1;
+        private System.Windows.Forms.Button load;
+        private System.Windows.Forms.Button generate;
     }
 }
--- oop-resumeCreator/Program.cs
+++ oop-resumeCreator/Program.cs
@@ -17,7 +17,7 @@ namespace oop_resumeCreator
             Application.SetHighDpiMode(HighDpiMode.SystemAware);
             Application.EnableVisualStyles();
             Application.SetCompatibleTextRenderingDefault(false);
-            Application.Run(new Form1());
+            Application.Run(new resume());
         }
     }
 }
